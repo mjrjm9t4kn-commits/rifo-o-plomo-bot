716018f03c1d9a626c820c3839c91217482a4eb8
mjrjm9t4kn-commits <mjrjm9t4kn@privaterelay.appleid.com>
2026-06-09 21:34:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -115,6 +115,34 @@ async def setup(ctx):
 """)
 
     await ctx.send("🔥 GOD RP serveur Rifo o Plomo créé !")
+# ================= Delet ==================
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def reset(ctx, confirm=None):
+
+    if confirm != "CONFIRMER":
+        return await ctx.send("⚠️ Tape `!reset CONFIRMER` pour supprimer tout le serveur.")
+
+    guild = ctx.guild
+
+    await ctx.send("💥 Reset du serveur en cours...")
+
+    # ================= DELETE CHANNELS =================
+    for channel in guild.channels:
+        try:
+            await channel.delete()
+        except:
+            pass
+
+    # ================= DELETE ROLES =================
+    for role in guild.roles:
+        if role.name != "@everyone":
+            try:
+                await role.delete()
+            except:
+                pass
+
+    await ctx.send("🔥 Reset terminé (salons + rôles supprimés)")
 
 # ================= TICKETS GOD SYSTEM =================
 class CloseTicketView(discord.ui.View):
@@ -162,31 +190,3 @@ async def on_member_join(member):
 
 # ================= RUN =================
 bot.run(TOKEN)
-# ================= Delet ==================
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def reset(ctx, confirm=None):
-
-    if confirm != "CONFIRMER":
-        return await ctx.send("⚠️ Tape `!reset CONFIRMER` pour supprimer tout le serveur.")
-
-    guild = ctx.guild
-
-    await ctx.send("💥 Reset du serveur en cours...")
-
-    # ================= DELETE CHANNELS =================
-    for channel in guild.channels:
-        try:
-            await channel.delete()
-        except:
-            pass
-
-    # ================= DELETE ROLES =================
-    for role in guild.roles:
-        if role.name != "@everyone":
-            try:
-                await role.delete()
-            except:
-                pass
-
-    await ctx.send("🔥 Reset terminé (salons + rôles supprimés)")
