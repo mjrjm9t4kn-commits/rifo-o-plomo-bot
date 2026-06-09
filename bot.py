09b2b4459a88d003d344ab137ad44749a4c10432
mjrjm9t4kn-commits <mjrjm9t4kn@privaterelay.appleid.com>
2026-06-09 19:36:53 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Connecté en tant que {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong !")
+
+bot.run(TOKEN)
